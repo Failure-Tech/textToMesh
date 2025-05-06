62064d251ea368f2a48c2db380bc57d64214fe74
Gautham <korrapatigautham@gmail.com>
2025-05-05 17:30:30 -0700

    contineu tmrw

---
+++ changenamelater/hybrid3d/img_to_3d.py
@@ -0,0 +1,6 @@
+# NeRF-based models: NeRF-w
+# Voxel-based/mesh autoencoders: CLIP-Forge, CLIP-Sculptor
+# DreamFusion for diffusion-based 3D Gen
+
+# Probably use CLIP-Forge (pre-trained) or implement custom
+
